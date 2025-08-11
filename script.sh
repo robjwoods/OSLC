550739ec2b2569716bf88c05588cb30524b6969d
robert.woods <robjwoods@me.com>
2025-08-11 20:04:00 +0100

    Refactor script for improved clarity and maintainability

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export AZDO_ORG="robjwoods"
+export AZDO_PROJECT="OSLC Test"
+export AZDO_PAT="BDbzVdmRicyVWmgLrr8XnZNJsm5WA6aDCs0Fck47WkP1eGK4ZUURJQQJ99BHACAAAAA9xJRNAAASAZDO1SrM"
+export AZDO_API_VER="7.0"
+
+python3 app.py
