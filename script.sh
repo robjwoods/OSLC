f44d6007614b26461635479a56bd9d890cf9db2c
robert.woods <robjwoods@me.com>
2025-08-13 05:48:06 +0100

    Removed script.sh

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export AZDO_ORG="robjwoods"
-export AZDO_PROJECT="OSLC Test"
-export AZDO_PAT="6veP8TX8oysueOwDERo0kkOrUrVZO42hnscrtoVBZOwLJyaUFOE4JQQJ99BHACAAAAA9xJRNAAASAZDO27Fq"
-export AZDO_API_VER="7.0"
-
-python3 app.py
